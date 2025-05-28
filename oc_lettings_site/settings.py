6d6bd9481746478e6407ba7054a2c84ec815d282
Gey <damien.gey74@gmail.com>
2025-05-28 11:36:42 +0200

    add gunicorn

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "p14-django-pipeline-ci-cd.onrender.com",]
 
 
 # Application definition
