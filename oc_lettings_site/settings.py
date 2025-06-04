adf83c817c1dca14eb5fd336059e722ce36675b7
Gey <damien.gey74@gmail.com>
2025-06-04 17:47:52 +0200

    collectstatic

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATIC_URL = "/static/"
@@ -127,7 +129,6 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 SENTRY_DSN = env("SENTRY_DSN", default=None)
 
