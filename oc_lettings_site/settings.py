cd065254b8b61c1a1e3cbd48dd36e19e8739efb1
Gey <damien.gey74@gmail.com>
2025-06-12 12:02:30 +0200

    testpipeline

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,11 +1,15 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 import environ
 
+
 env = environ.Env()
 environ.Env.read_env()
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -131,7 +135,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-SENTRY_DSN = env("SENTRY_DSN", default=None)
+SENTRY_DSN = os.getenv("SENTRY_DSN", default=None)
 
 sentry_sdk.init(
     dsn=SENTRY_DSN,
