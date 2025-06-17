2fe087e422337fbf43c370aaea69d0a00057d946
Gey <damien.gey74@gmail.com>
2025-06-17 12:10:16 +0200

    test_pipline

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -4,10 +4,16 @@ from django.urls import path, include
 from oc_lettings_site import tests
 from . import views
 
+def sentry_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("lettings/", include("lettings.urls")),
     path("profiles/", include("profiles.urls")),
     path("admin/", admin.site.urls),
     path("500/", tests.test_500),
+    path('sentry-debug/', sentry_error),
 ]
+
